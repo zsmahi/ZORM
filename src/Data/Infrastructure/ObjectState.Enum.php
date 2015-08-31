46302e0a1f87b5baccd16c227e23a9eb8fe58913
Zakaria SMAHI <zakaria08esi@gmail.com>
2015-08-31 22:56:19 +0100

    Initial Commit

---
+++ src/Data/Infrastructure/ObjectState.Enum.php
@@ -0,0 +1,14 @@
+<?php
+namespace ZORM\Data\Infrastrcture;
+/**
+ *
+ * Enum ObjectState, this enum is the enumeration of the different object states of an entity
+ *
+*/
+abstract class ObjectState{
+    const Unchanged = 0;
+    const Added = 1;
+    const Modified = 2;
+    const Deleted = -1;
+}
+?>
