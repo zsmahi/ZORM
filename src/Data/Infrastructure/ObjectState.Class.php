34cf28888369f7ce8fb0084a8354a9181b12c60e
Zakaria SMAHI <zakaria08esi@gmail.com>
2015-08-31 23:20:20 +0100

    Addinig initial ObjectState

---
+++ src/Data/Infrastructure/ObjectState.Class.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ZORM\Data\Infrastrcture;
+
+/**
+ *
+ * ObjectState abstract class, all mapped or not OnjectState are here
+ *
+ */
+
+public abstract ObjectState{
+
+    private ObjectStateEnum $__objectState;
+    public function __construct(){
+
+    }
+    public function __destruct(){
+
+    }
+    public function GetObjectState(){
+
+    }
+
+    public function SetObjectState(){
+
+    }
+
+}
+?>
--- src/Data/Infrastructure/ObjectState.Enum.php
+++ src/Data/Infrastructure/ObjectState.Enum.php
@@ -5,7 +5,7 @@ namespace ZORM\Data\Infrastrcture;
  * Enum ObjectState, this enum is the enumeration of the different object states of an entity
  *
 */
-abstract class ObjectState{
+abstract class ObjectStateEnum{
     const Unchanged = 0;
     const Added = 1;
     const Modified = 2;
